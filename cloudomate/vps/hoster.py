398d5a3f5d6bd59d5067ae3fcefa1a8caf810fa7
TheFrog <aizokroon@hotmail.com>
2017-12-03 18:08:29 +0100

    Updated solusvm_hoster for mechanicalsoup and fixed pulse servers

--- cloudomate/vps/hoster.py
+++ cloudomate/vps/hoster.py
@@ -12,33 +12,33 @@ from mechanicalsoup import StatefulBrowser
 
 from cloudomate import wallet as wallet_util
 
-user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
-]
-
 
 class Hoster(object):
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
+    ]
+
     name = None
     website = None
     required_settings = None
@@ -145,9 +145,9 @@ class Hoster(object):
                 price_string = 'est. unavailable'
             yield i, item, estimated_price, price_string
 
-    @staticmethod
-    def _create_browser():
-        return StatefulBrowser(user_agent=random.choice(user_agents))
+    @classmethod
+    def _create_browser(cls):
+        return StatefulBrowser(user_agent=random.choice(cls.user_agents))
 
     @staticmethod
     def _open_in_browser(page):
--- cloudomate/vps/solusvm_hoster.py
+++ cloudomate/vps/solusvm_hoster.py
@@ -52,26 +52,32 @@ class SolusvmHoster(Hoster):
         :param user_settings: settings
         :return: 
         """
-        br.form['firstname'] = user_settings.get("firstname")
-        br.form['lastname'] = user_settings.get("lastname")
-        br.form['email'] = user_settings.get("email")
-        br.form['phonenumber'] = user_settings.get("phonenumber")
-        br.form['companyname'] = user_settings.get("companyname")
-        br.form['address1'] = user_settings.get("address")
-        br.form['city'] = user_settings.get("city")
-        br.form['state'] = user_settings.get("state")
-        br.form['postcode'] = user_settings.get("zipcode")
-        br.form['country'] = [user_settings.get('countrycode')]
-        br.form['password'] = user_settings.get("password")
-        br.form['password2'] = user_settings.get("password")
-        br.form['paymentmethod'] = [payment_method]
+        form = br.get_current_form()
+        form['firstname'] = user_settings.get("firstname")
+        form['lastname'] = user_settings.get("lastname")
+        form['email'] = user_settings.get("email")
+        form['phonenumber'] = user_settings.get("phonenumber")
+        form['companyname'] = user_settings.get("companyname")
+        form['address1'] = user_settings.get("address")
+        form['city'] = user_settings.get("city")
+        form['state'] = user_settings.get("state")
+        form['postcode'] = user_settings.get("zipcode")
+        #form['country'] = [user_settings.get('countrycode')]
+        #form.set('country', [user_settings.get('countrycode')])
+        form['country'] = user_settings.get('countrycode')
+        form['password'] = user_settings.get("password")
+        form['password2'] = user_settings.get("password")
+        form['paymentmethod'] = payment_method
         if acceptos:
-            br.find_control('accepttos').items[0].selected = True
+            form['accepttos'] = True
 
-        page = br.submit()
-        if 'checkout' in page.geturl():
-            contents = BeautifulSoup(page.read(), 'lxml')
-            errors = contents.find('div', {'class': errorbox_class}).text
+        soup = br.get_current_page()
+        submit = soup.select('input.ordernow')[0]
+        page = br.submit_selected(submit)
+
+        if 'checkout' in page.url:
+            soup = BeautifulSoup(page.text, 'lxml')
+            errors = soup.find('div', {'class': errorbox_class}).text
             print((errors.strip()))
             sys.exit(2)
 
