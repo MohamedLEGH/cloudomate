903e967b7566dcafcfc270dd789a39016e9dfadb
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2017-12-19 12:29:09 +0100

    Add lxml as pip dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
 
     packages=find_packages(exclude=['docs', 'test']),
 
-    install_requires=['appdirs', 'MechanicalSoup', 'bs4', 'mock', 'forex-python', 'parameterized'],
+    install_requires=['appdirs', 'lxml', 'MechanicalSoup', 'bs4', 'mock', 'forex-python', 'parameterized'],
 
     extras_require={
         'dev': [],
