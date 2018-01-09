9761711b4fb92a7c3599dcbeab3979b9b6384392
Joost Wooning <jwooning@gmail.com>
2018-01-09 16:02:52 +0100

    Tests for purchasing VPS server, currently test fails because bitpay payment extraction fails

--- cloudomate/test/test_hoster.py
+++ cloudomate/test/test_hoster.py
@@ -1,7 +1,8 @@
 import unittest
 
-import cloudomate.hoster.vps.vps_hoster
-from cloudomate import wallet
+from parameterized import parameterized
+from mock.mock import MagicMock
+
 from cloudomate.hoster.vps.blueangelhost import BlueAngelHost
 from cloudomate.hoster.vps.ccihosting import CCIHosting
 from cloudomate.hoster.vps.crowncloud import CrownCloud
@@ -9,9 +10,8 @@ from cloudomate.hoster.vps.legionbox import LegionBox
 from cloudomate.hoster.vps.linevast import LineVast
 from cloudomate.hoster.vps.pulseservers import Pulseservers
 from cloudomate.hoster.vps.undergroundprivate import UndergroundPrivate
-from cloudomate.hoster.vps.vpsoption import VpsOption
-from mock.mock import MagicMock
-from parameterized import parameterized
+
+from cloudomate.util.fakeuserscraper import UserScraper
 
 providers = [
     (BlueAngelHost,),
@@ -35,46 +35,17 @@ class TestHosters(unittest.TestCase):
         options = hoster().start()
         self.assertTrue(len(list(options)) > 0)
 
+    @parameterized.expand(providers)
+    def test_hoster_purchase(self, hoster):
+        user_settings = UserScraper().get_user()
+        host = hoster()
+        options = list(host.start())[0]
+        wallet = MagicMock()
+        wallet.pay = MagicMock()
 
-class TestHosterAbstract(unittest.TestCase):
-    def test_hoster_options(self):
-        hoster = cloudomate.hoster.hoster.Hoster()
-        self.assertRaises(NotImplementedError, hoster.options)
+        host.purchase(user_settings, options, wallet)
 
-    def test_hoster_purchase(self):
-        hoster = cloudomate.hoster.hoster.Hoster()
-        vps_option = VpsOption(name='', price='', cpu='', currency='USD', ram='', storage='', bandwidth='',
-                               connection='', purchase_url='')
-        self.assertRaises(NotImplementedError, hoster.purchase, *(None, vps_option, None))
-
-    def test_hoster_print(self):
-        hoster = cloudomate.hoster.hoster.Hoster()
-        options = [self._create_option()]
-        hoster.configurations = options
-        wallet.get_rates = MagicMock(return_value={'USD': 1.1})
-        hoster.gateway = MagicMock()
-        hoster.gateway.estimate_price.return_value = 1.2
-        hoster.print_configurations()
-        wallet.get_rates.assert_called_once()
-        hoster.gateway.estimate_price.assert_called_once()
-
-    def test_create_browser(self):
-        hoster = cloudomate.hoster.hoster.Hoster()
-        self.assertRaises(NotImplementedError, hoster.options)
-
-    @staticmethod
-    def _create_option():
-        return VpsOption(
-            name="Option name",
-            ram="Option ram",
-            cpu="Option cpu",
-            storage="Option storage",
-            bandwidth="Option bandwidth",
-            price=12,
-            currency="USD",
-            connection="Option connection",
-            purchase_url="Option url"
-        )
+        wallet.pay.assert_called_once()
 
 
 if __name__ == '__main__':
