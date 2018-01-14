554e3ddb0dea55293b80252281f3e6c25de7766e
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2018-01-15 00:00:51 +0100

    ethereum wallet

--- cloudomate/wallet.py
+++ cloudomate/wallet.py
@@ -7,12 +7,23 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-from forex_python.bitcoin import BtcConverter
 from mechanicalsoup import StatefulBrowser
 
-AVG_TX_SIZE = 226
-SATOSHI_TO_BTC = 0.00000001
+# library for currency prices
+from cryptocompy import price
 
+# library used for ethereum
+import rlp 
+
+from web3 import Web3, HTTPProvider, IPCProvider
+from ethereum.transactions import Transaction
+
+
+NB_GAS_FOR_TRANSACTION = 21000
+# get gas price in gas station ??
+
+ETHER_TO_WEI=1000000000000000000
+WEI_TO_ETHER=0.000000000000000001
 
 def determine_currency(text):
     """
@@ -29,33 +40,23 @@ def determine_currency(text):
         return None
 
 
-def get_rate(currency='USD'):
+def get_rate(currency="USD"):
     """
-    Return price of 1 currency in BTC
-    Supported currencies available at 
-    http://forex-python.readthedocs.io/en/latest/currencysource.html#list-of-supported-currency-codes
+    Return price of 1 currency in ETH
+
     :param currency: currency to convert to
-    :return: conversion rate from currency to BTC
+    :return: conversion rate from currency to ETH
     """
     if currency is None:
         return None
-    b = BtcConverter()
-    factor = b.get_latest_price(currency)
-    if factor is None:
-        factor = 1.0 / fallback_get_rate(currency)
+    factor = price.get_current_price("ETH",currency)
+ 
     return 1.0 / factor
-
-
-def fallback_get_rate(currency):
-    # Sometimes the method above gets rate limited, in this case use
-    # https: // blockchain.info / tobtc?currency = USD & value = 500
-    return float(urllib.request.urlopen('https://blockchain.info/tobtc?currency={0}&value=1'.format(currency)).read())
-
 
 def get_rates(currencies):
     """
-    Return rates for all currencies to BTC.
-    :return: conversion rates from currencies to BTC
+    Return rates for all currencies to ETH.
+    :return: conversion rates from currencies to ETH
     """
     rates = {cur: get_rate(cur) for cur in currencies}
     return rates
@@ -63,31 +64,25 @@ def get_rates(currencies):
 
 def get_price(amount, currency='USD'):
     """
-    Convert price from one currency to bitcoins
+    Convert price from one currency to ether
     :param amount: number of currencies to convert
     :param currency: currency to convert from
-    :return: price in bitcoins
+    :return: price in ether
     """
     price = amount * get_rate(currency)
     return price
 
-
-def _get_network_cost(speed):
-    br = StatefulBrowser(user_agent='Firefox')
-    page = br.open('https://bitcoinfees.21.co/api/v1/fees/recommended')
-    response = page.json()
-    satoshirate = float(response[speed])
-    return satoshirate
-
-
-def get_network_fee(speed='halfHourFee'):
+def get_network_fee(): # with web3.py he gives 520 gwei which is too much
     """
-    Give an estimate of network fee for the average bitcoin transaction for given speed.
-    Supported speeds are available at https://bitcoinfees.21.co/api/v1/fees/recommended
+    Give an estimate of network fee for a simple ether transaction.
+    from http://gasprice.dopedapp.com/
     :return: network cost
     """
-    network_fee = _get_network_cost(speed) * SATOSHI_TO_BTC
-    return network_fee * AVG_TX_SIZE
+    br = StatefulBrowser(user_agent='Firefox')
+    page = br.open('http://gasprice.dopedapp.com/')
+    response = page.json()
+    gwei_cost = float(response['safe_price_in_gwei'])
+    return gwei_cost
 
 
 class Wallet:
@@ -97,139 +92,50 @@ class Wallet:
     Wallets with passwords may still be used, but passwords will have to be entered manually.
     """
 
-    def __init__(self, wallet_command=None, wallet_path=None):
-        if wallet_command is None:
-            if os.path.exists('/usr/local/bin/electrum'):
-                wallet_command = ['/usr/local/bin/electrum']
-            else:
-                wallet_command = ['/usr/bin/env', 'electrum']
-        self.command = wallet_command
-        self.wallet_handler = ElectrumWalletHandler(wallet_command, wallet_path)
+    def __init__(self, private_key, provider):
 
-    def get_balance(self, confirmed=True, unconfirmed=True):
-        """
-        Return the balance of the default electrum wallet
-        Confirmed and unconfirmed can be set to indicate which balance to retrieve.
-        :param confirmed: default: True
-        :param unconfirmed: default: True
-        :return: balance of default wallet
-        """
-        balance_output = self.wallet_handler.get_balance()
-        balance = 0.0
-        if confirmed:
-            balance = balance + float(balance_output.get('confirmed', 0.0))
-        if unconfirmed:
-            balance = balance + float(balance_output.get('unconfirmed', 0.0))
-        return balance
-
-    def get_balance_confirmed(self):
-        """
-        Return confirmed balance of default electrum wallet
-        :return: 
-        """
-        return self.get_balance(confirmed=True, unconfirmed=False)
-
-    def get_balance_unconfirmed(self):
-        """
-        Return unconfirmed balance of default electrum wallet
-        :return: 
-        """
-        return self.get_balance(confirmed=False, unconfirmed=True)
-
-    def get_addresses(self):
-        """
-        Return the list of addresses of the default electrum wallet
-        :return: 
-        """
-        address_output = self.wallet_handler.get_addresses()
-        return address_output
-
-    def pay(self, address, amount, fee=None):
-        tx_fee = 0 if fee is None else fee
-        if self.get_balance() < amount + tx_fee:
-            print('Not enough funds')
-            return
-        transaction_hex = self.wallet_handler.create_transaction(amount, address, fee)
-        success, transaction_hash = self.wallet_handler.broadcast(transaction_hex)
-        if not success:
-            print(('Transaction not successfully broadcast, do error handling: {0}'.format(transaction_hash)))
-        else:
-            print('Transaction successful')
-        print(transaction_hex)
-        print(success)
-        print(transaction_hash)
-        return transaction_hash
-
-
-class ElectrumWalletHandler(object):
-    """
-    ElectrumWalletHandler ensures the correct opening and closing of the electrum wallet daemon
-    """
-
-    def __init__(self, wallet_command=None, wallet_path=None):
-        """
-        Allows wallet_command to be changed to for example electrum --testnet
-        :param wallet_command: command to call wallet
-        """
-        if wallet_command is None:
-            if os.path.exists('/usr/local/bin/electrum'):
-                wallet_command = ['/usr/local/bin/electrum']
-            else:
-                wallet_command = ['/usr/bin/env', 'electrum']
-        self.command = wallet_command
-        p, e = subprocess.Popen(self.command + ['daemon', 'status'], stdout=subprocess.PIPE).communicate()
-        self.not_running_before = b'not running' in p
-        if self.not_running_before:
-            subprocess.call(self.command + ['daemon', 'start'])
-        if wallet_path is None:
-            subprocess.call(self.command + ['daemon', 'load_wallet'])
-        else:
-            print('Using wallet: ', wallet_path)
-            subprocess.call(self.command + ['daemon', 'load_wallet', '-w', wallet_path])
-
-    def __del__(self):
-        if self.not_running_before:
-            subprocess.call(self.command + ['daemon', 'stop'])
-
-    def create_transaction(self, amount, address, fee):
-        """
-        Create a transaction
-        :param amount: amount of bitcoins to be transferred
-        :param address: address to transfer to
-        :param fee: None for autofee, or specify own fee
-        :return: transaction details
-        """
-        if fee is None:
-            transaction = subprocess.check_output(self.command + ['payto', str(address), str(amount)]).decode()
-        else:
-            transaction = subprocess.check_output(self.command + ['payto', str(address), str(amount), '-f', str(fee)]).decode()
-        jtrs = json.loads(transaction)
-        return jtrs['hex']
-
-    def broadcast(self, transaction):
-        """
-        Broadcast a transaction
-        :param transaction: hex of transaction
-        :return: if successful returns success and
-        """
-        broadcast = subprocess.check_output(self.command + ['broadcast', transaction]).decode()
-        jbr = json.loads(broadcast)
-        return tuple(jbr)
+        	self.web3 = Web3(HTTPProvider(provider))
+        	assert web3.isConnected()
+        	self.key = private_key
+        	self.address = ethereum.utils.privtoaddr(private_key)
+		assert web3.isAddress(self.address)
 
     def get_balance(self):
         """
-        Return the balance of the default electrum wallet
-        :return: balance of default wallet
-        """
-        output = subprocess.check_output(self.command + ['getbalance']).decode()
-        balance_dict = json.loads(output)
-        return balance_dict
+        Return the balance of the address
 
-    def get_addresses(self):
         """
-        Return the list of addresses of default wallet
-        :return: 
-        """
-        address = subprocess.check_output(self.command + ['listaddresses']).decode()
-        addr = json.loads(address)
-        return addr
+        assert web3.isConnected()
+        balance = web3.eth.getBalance(self.address) 
+
+        return balance
+
+
+    def pay(self, addressToSend, amount, fee=get_network_fee(),number_gas=NB_GAS_FOR_TRANSACTION): 
+    	"""
+    	Function to send ether to an address, you can choose the fees
+    	
+    	"""
+	assert web3.isConnected()
+	nonceAddress=web3.eth.getTransactionCount(self.address)
+	assert web3.isAddress(addressToSend)
+	
+	amount_In_Wei = amount*ETHER_TO_WEI
+	fee_In_Wei = fee*10**9 # 1 Gwei = 1 billion wei
+	
+	tx = Transaction(
+	nonce = nonceAddress,
+	gasprice = fee_In_Wei, 
+	startgas = number_gas,
+	to = addressToSend,
+	value = amount_In_Wei,
+	data = b'', # no need of additional data
+	)
+	tx.sign(self.key)
+	raw_tx = rlp.encode(tx)
+	raw_tx_hex = web3.toHex(raw_tx)
+	txHash = web3.eth.sendRawTransaction(raw_tx_hex)
+	
+	return txHash
+
+
