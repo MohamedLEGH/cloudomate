41f3849d845d960690568f04cacf1efebb954191
kw <kawing.man@smartinstall.nl>
2018-02-02 16:29:31 +0100

    Small change

--- cloudomate/util/captchareload.py
+++ cloudomate/util/captchareload.py
@@ -1,40 +1,88 @@
 import time
 import re
+import sys
 from selenium import webdriver
+from seleniumrequests import Chrome
+import mechanicalsoup
+from cloudomate.util.recaptchasolver import reCaptchaSolver
 
 class anticaptcha:
 
-    username = ""
-    password = ""
+    username = "ralphiek"
+    password = "Hoerenzoon2@"
 
     # Use this method for purchasing with Bitcoin.
     def purchase_bitcoin(self, amount=10):
-        return self.purchase(["Bitcoin", "BTC", "btc"], amount)
+        try:
+            return self._purchase(["Bitcoin", "BTC", "btc"], amount)
+        except Exception as e:
+            print("Error " + str(e) + "Try again. It it still does not work, "
+                                      "website might have been updated, update script.")
 
     # Use this method for purchasing with Litecoin.
     def purchase_litecoin(self, amount=10):
-        return self.purchase(["Litecoin", "LTC", "ltc"], amount)
+        try:
+            return self._purchase(["Litecoin", "LTC", "ltc"], amount)
+        except Exception:
+            print("Something went wrong. Try again. It it still does not work, update script.")
 
     # Use this method for purchasing with DASH.
     def purchase_dash(self, amount=10):
-        return self.purchase(["DASH", "DASH", "dash"], amount)
+        try:
+            return self._purchase(["DASH", "DASH", "dash"], amount)
+        except Exception:
+            print("Something went wrong. Try again. It it still does not work, update script.")
 
     # Don't invoke this method directly.
-    def purchase(self, currency, amount=10):
+    def _purchase(self, currency, amount=10):
+        # Does the CAPTCHA
+        #browser = mechanicalsoup.StatefulBrowser()
+        link = "http://http.myjino.ru/recaptcha/test-get.php"
+        #browser.open(link)
+        #page = str(browser.get_current_page())
+        #match = re.findall('data-sitekey="(.*?)"', page)
+        #print("match = " + match[0])
+        #captchasolver = reCaptchaSolver("fd58e13e22604e820052b44611d61d6c")
+        #solution = captchasolver.solveGoogleReCaptcha(link, match[0])
+        #browser.open("http://http.myjino.ru/recaptcha/test-get.php?text=asdasd&g-recaptcha-response=" + solution)
+        #print(str(browser.get_current_page()))
+
         # Selenium setup: headless Chrome, Window size needs to be big enough, otherwise elements will not be found.
         options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('disable-gpu');
-        options.add_argument('window-size=1920,1080');
+        #options.add_argument('headless')
+        #options.add_argument('disable-gpu');
+        #options.add_argument('window-size=1920,1080');
         driver = webdriver.Chrome(chrome_options=options)
         driver.maximize_window()
 
-        #Logs in
+        driver.get(link)
+        data = driver.find_element_by_class_name("g-recaptcha").get_attribute("data-sitekey")
+        captchasolver = reCaptchaSolver("fd58e13e22604e820052b44611d61d6c")
+        solution = captchasolver.solveGoogleReCaptcha(link, data)
+        driver.get("http://http.myjino.ru/recaptcha/test-get.php?text=hallo&g-recaptcha-response=" + solution)
+        print(driver.page_source)
+
+        # Logs in
         driver.get("https://anti-captcha.com/clients/entrance/login")
         driver.find_element_by_id('enterlogin').send_keys(self.username)
         driver.find_element_by_id('password').send_keys(self.password)
         driver.find_element_by_css_selector("button.btn.btn-primary").click()
         time.sleep(2)
+
+        # Captcha (if any)
+        captcha_element = None
+        try:
+            captcha_element = driver.find_element_by_id('recaptchaForm')
+            if (captcha_element is not None):
+                captcha_element = captcha_element.find_element_by_class_name('g-recaptcha')
+                data_sitekey = captcha_element.get_attribute('data-sitekey')
+                print(data_sitekey)
+                sys.exit(0)
+                response = webdriver.request('POST', )
+                print(response)
+        except Exception: # If there is no CAPTCHA to solve.
+            pass
+
 
         # Clicking menu
         print("Click Finance")
@@ -100,3 +148,11 @@ class anticaptcha:
         amount = re.findall("send (.*?)" + currency[1] + " to", instructions.text)[0]
 
         return {'amount' : str(amount), 'address' : str(address)}
+
+if __name__ == "__main__":
+    captchasolver = reCaptchaSolver("fd58e13e22604e820052b44611d61d6c")
+    solution = captchasolver.solveGoogleReCaptcha("https://anti-captcha.com/clients/entrance/login", "6LfydQgUAAAAAMuh1gRreQdKjAop7eGmi6TrNIzp")
+    sys.exit(0)
+
+    a = anticaptcha()
+    a.purchase_bitcoin()
